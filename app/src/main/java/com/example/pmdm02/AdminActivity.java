3e00cfbc2fae72e70efd2130e8181c50a2a17282
Javier Fernández Ferrol <duchecw@gmail.com>
2019-11-02 20:59:22 +0100

    Versión Final

--- app/src/main/java/com/example/pmdm02/AdminActivity.java
+++ app/src/main/java/com/example/pmdm02/AdminActivity.java
@@ -2,9 +2,7 @@ package com.example.pmdm02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -12,10 +10,5 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-    }
-    public void salir(View view){
-        Intent login = new Intent(this, MainActivity.class);
-        login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(login);
     }
 }
